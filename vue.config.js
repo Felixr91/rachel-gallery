64046723a5f96db92db2c6fc5d07e82ff777fad4
Felix <felixr91@gmail.com>
2019-02-16 09:02:26 -0700

    vue configg

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/rachel-gallery/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/rachel-gallery/'
+  //   : '/',
+
+  baseUrl: '/rachel-gallery',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
