a404231e16b99e8307f7e2b445bac3ba2eca72bb
Felix <felixr91@gmail.com>
2019-03-30 00:41:44 -0600

    svghands

--- src/components/heroImage.vue
+++ src/components/heroImage.vue
@@ -20,16 +20,31 @@
 </script>
 
 <style>
-  @media only screen and (min-width: 720px) {
+  @media only screen and (min-width: 920px) {
     .handdiv {
       background-repeat: no-repeat;
       background-size: 80vw;
-      background-image: url("../assets/naturalmythologystamphands.png");
-      background-position: center;
+      background-image: url("../assets/hands.svg");
+      background-position: 50% 70%;
     }
   }
 
-  @media only screen and (max-width: 719px) {
+  @media only screen and (min-width: 1500px) and (min-height: 600px) {
+    .handdiv {
+      background-repeat: no-repeat;
+      background-size: 70vw;
+      background-image: url("../assets/hands.svg");
+      background-position: 50% 90%;
+    }
+  }
+
+  @media only screen and (max-height: 800px) {
+    .handdiv {
+      height: 800px;
+    }
+  }
+
+  @media only screen and (max-width: 919px) {
     .handdiv {
       background-repeat: no-repeat;
       background-size: 300px;
@@ -38,12 +53,6 @@
     }
   }
 
-  @media only screen and (max-height: 400px) {
-    .hero-query {
-      height: 400px;
-      /* margin-top: 100px; */
-    }
-  }
 
   @media only screen and (min-height: 401px) {
     .hero-query {
